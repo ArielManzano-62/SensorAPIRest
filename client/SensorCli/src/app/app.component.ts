2d1f6d8af60cc76ea6e959e5f3a452d5e6884535
ArielManzano-62 <ariel.manzano0602@gmail.com>
2021-05-01 12:29:31 -0300

    Pantallita de sensor fea

--- client/SensorCli/src/app/app.component.ts
+++ client/SensorCli/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Sensor } from './models/sensor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'SensorCli';
+  sensors: Sensor[] = [
+    { id: 1, name: 'Sensor 1' },
+    { id: 2, name: 'Sensor 2' },
+    { id: 3, name: 'Sensor 3' },
+  ];
 }
